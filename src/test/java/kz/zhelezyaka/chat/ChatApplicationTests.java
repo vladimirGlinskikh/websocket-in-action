aef3512db36d529b7b4528948c215361c5c10d40
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2024-02-22 16:34:46 +0300

    Start Project

---
+++ src/test/java/kz/zhelezyaka/chat/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
